a28606825592a0faf286d59aa275327f49922365
Sorina Alexandrescu <sorina.alexandrescu5@gmail.com>
2021-01-02 22:16:17 +0000

    cart finished and styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,22 @@
 Rails.application.routes.draw do
+
+  get 'cart/index'
   
+  resources :products
   root 'static_pages#home'
+# get '/home' => 'static_pages#home'
+  get '/products', to: 'static_pages#products'
+  get '/about', to: 'static_pages#about'
+  get '/contact', to:'static_pages#contact'
 
-  get '/home' => 'static_pages#home'
-  get '/about' => 'static_pages#about'
-  get '/contact' => 'static_pages#contact'
+  get '/login', to:'user# login'
+  get '/logout', to:'user#logout'
 
-  get '/login' => 'user# login'
-  get '/logout' => 'user#logout'
-
+  get '/cart/clear', to: 'cart#clear'
   get '/cart', to: 'cart#index'
   get '/cart/:id', to: 'cart#add'
-  resources :products
-  # get 'static_pages/Home'
-  # get 'static_pages/About'
-  # get 'static_pages/Contact'
+  get '/cart/remove/:id', to:'cart#remove'
+ 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
